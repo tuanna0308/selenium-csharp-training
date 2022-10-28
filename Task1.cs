83991a7866d9d2a24675f1b4a66e1082a1ab990e
Tuan_Nguyen4 <Tuan_Nguyen4@epam.com>
2022-10-28 11:18:52 +0700

    Update WebDriverManager and add new folders for TAF

--- Task1.cs
+++ Task1.cs
@@ -3,6 +3,8 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Opera;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace TrainingCsharpSelenium
 {
@@ -10,25 +12,42 @@ namespace TrainingCsharpSelenium
     {
         IWebDriver driver;
 
+        NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         [SetUp]
         public void Setup()
         {
-            string driverPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\browserDrivers\";
-            
-            driver = new ChromeDriver(driverPath);
+            logger.Info("Set up");
+
+            // string driverPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\browserDrivers\";
+
+            // driver = new ChromeDriver(driverPath);
             // driver = new FirefoxDriver(driverPath);
             // driver = new OperaDriver(driverPath);
             // driver = new EdgeDriver(driverPath);
+
+            //new DriverManager().SetUpDriver(new ChromeConfig());
+            new DriverManager().SetUpDriver(new FirefoxConfig());
+            //new DriverManager().SetUpDriver(new OperaConfig());
+            //new DriverManager().SetUpDriver(new EdgeConfig());
+
+            //driver = new ChromeDriver();
+            driver = new FirefoxDriver();
+            //driver = new OperaDriver();
+            //driver = new EdgeDriver();
 
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
             driver.Navigate().GoToUrl("https://www.google.com/");
+            Assert.True(driver.Title.Contains("Google"));
         }
 
         [Test]
         public void Test1()
         {
+            logger.Info("Test");
+
             IWebElement searchTextbox;
             string searchText = "C# Tutorial";
 
@@ -53,6 +72,8 @@ namespace TrainingCsharpSelenium
         [TearDown]
         public void TearDown()
         {
+            logger.Info("Tear Down");
+            NLog.LogManager.Shutdown();
             driver.Quit();
         }
     }
