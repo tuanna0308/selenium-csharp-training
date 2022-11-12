b2c41fb4579d53dd714e5c977010f1b4229ed066
Tuan_Nguyen4 <Tuan_Nguyen4@epam.com>
2022-11-12 15:55:13 +0700

    Change to test Jenkins

--- Tests/TestMain.cs
+++ Tests/TestMain.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using PageObjectBase.Commons;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TrainingCsharpSeleniumHomework.Tests
 {
-    public class TestMain
+    public class TestMain : BaseTest
     {
-        static void Main(string[] args) { }
+        static void Main(string[] args)
+        {
+            Log.Info("Test Main to test Jenkins");
+            Log.Info("ConfigurationManager.AppSettings[GlobalConstants.BROWSER].ToUpper(): " + ConfigurationManager.AppSettings[GlobalConstants.BROWSER].ToUpper());
+            Console.WriteLine("Console: ConfigurationManager.AppSettings[GlobalConstants.BROWSER].ToUpper(): " + ConfigurationManager.AppSettings[GlobalConstants.BROWSER].ToUpper());
+        }
     }
 }
